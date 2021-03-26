7d383746baf550cd5d18f1c2c84387a14c4f18e5
Chris Lee <chrislee011502@gmail.com>
2021-03-26 03:40:22 -0700

    Update gsParser.py

--- App/scripts/gsParser.py
+++ App/scripts/gsParser.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 from lxml import html
 
 
-
 session_requests = requests.session()
 login_url = "https://www.gradescope.com/login"
 result = session_requests.get(login_url)
@@ -31,16 +30,46 @@ result = session_requests.get(
 c = result.content
 soup = BeautifulSoup(c, features="lxml")
 
-raw_course_names = soup.find_all('h4', {"class": "courseBox--name"})
-
+# Cteates a list of course names
 course_names = []
+raw_course_names = soup.find_all('h3', {"class": "courseBox--shortname"})
 
 for each in raw_course_names:
     each = str(each)
     
-    new_each = each.lstrip('<h4 class="courseBox--name">')
-    new_each = new_each.rstrip('</h4>')
+    new_each = each.lstrip('<h3 class="courseBox--shortname">')
+    new_each = new_each.rstrip('</h3>')
     course_names.append(new_each)
 
-print(course_names)
+# Creates a list of the unique course number identifier to be used in the course url
+course_numbers = []
+raw_course_numbers = soup.find_all('a', {"class": "courseBox"}, )
 
+for each in raw_course_numbers:
+    each = str(each)
+
+    new_each = each[36:42]
+    course_numbers.append(new_each)
+
+# Creates a list of unique course urls
+course_urls = []
+url_header = "https://www.gradescope.com/courses/"
+
+for each in course_numbers:
+    url = url_header + each
+    course_urls.append(url)
+
+# Creates a dictionary with the key-value pair of {course_name : list_of_assignments}
+course_with_assignments = {}
+
+for each in course_names:
+    course_with_assignments[each] = None
+
+# Here, we want to go through each course page and create a list of assignments for each page
+# We can add these assignments to a list and then add this list to their respective place in the dictionary course_with_assignments
+#for each in course_urls:
+#    
+#    result = session_requests.get(
+#        each,
+#        headers = dict(referer = each)
+#    )
